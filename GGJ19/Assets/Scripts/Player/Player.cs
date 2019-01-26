823a25eb32efbd36e260ce4c4cb735899e91e692
Alberto Paz Becerril <alberto.paz@mediatonic.co.uk>
2019-01-26 03:38:20 +0100

    Create player class with karma.

---
+++ GGJ19/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Assets.Scripts.Player
+{
+    public class Player
+    {
+        private const int MinKarma = 0;
+        private const int MaxKarma = 100;
+
+        public int Karma { get; private set; }
+
+        public void IncreaseKarma(int quantity)
+        {
+            Karma = Math.Min(Karma + quantity, MaxKarma);
+        }
+
+        public void ReduceKarma(int quantity)
+        {
+            Karma = Math.Max(Karma - quantity, MinKarma);
+        }
+    }
+}
