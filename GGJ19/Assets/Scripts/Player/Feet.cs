23a4a6e96b3430088778550dfbcac09615097d71
Alberto Paz Becerril <alberto.paz@mediatonic.co.uk>
2019-02-04 09:04:19 +0100

    Change colliders approach to use a trigger on the character's feet.

---
+++ GGJ19/Assets/Scripts/Player/Feet.cs
@@ -0,0 +1,84 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class Feet : MonoBehaviour
+    {
+        public bool AllowVerticalMovementUp { get; set; }
+        public bool AllowVerticalMovementDown { get; set; }
+        public bool AllowHorizontalMovementLeft { get; set; }
+        public bool AllowHorizontalMovementRight { get; set; }
+
+        void Start()
+        {
+            AllowVerticalMovementUp = true;
+            AllowVerticalMovementDown = true;
+            AllowHorizontalMovementLeft = true;
+            AllowHorizontalMovementRight = true;
+        }
+
+        void OnTriggerEnter2D(Collider2D collider)
+        {
+            switch (collider.tag)
+            {
+                case Tags.VerticalTriggerUp:
+                    AllowVerticalMovementUp = false;
+                    break;
+                case Tags.VerticalTriggerDown:
+                    AllowVerticalMovementDown = false;
+                    break;
+                case Tags.HorizontalTriggerLeft:
+                    AllowHorizontalMovementLeft = false;
+                    break;
+                case Tags.HorizontalTriggerRight:
+                    AllowHorizontalMovementRight = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void OnTriggerStay2D(Collider2D collider)
+        {
+            switch (collider.tag)
+            {
+                case Tags.VerticalTriggerUp:
+                    AllowVerticalMovementUp = false;
+                    break;
+                case Tags.VerticalTriggerDown:
+                    AllowVerticalMovementDown = false;
+                    break;
+                case Tags.HorizontalTriggerLeft:
+                    AllowHorizontalMovementLeft = false;
+                    break;
+                case Tags.HorizontalTriggerRight:
+                    AllowHorizontalMovementRight = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        void OnTriggerExit2D(Collider2D collider)
+        {
+            switch (collider.tag)
+            {
+                case Tags.VerticalTriggerUp:
+                    AllowVerticalMovementUp = true;
+                    break;
+                case Tags.VerticalTriggerDown:
+                    AllowVerticalMovementDown = true;
+                    break;
+                case Tags.HorizontalTriggerLeft:
+                    AllowHorizontalMovementLeft = true;
+                    break;
+                case Tags.HorizontalTriggerRight:
+                    AllowHorizontalMovementRight = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
