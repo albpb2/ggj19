fa1cc48f60c8f54eae1e1ad238cfa274b8948e5f
Alberto Paz Becerril <alberto.paz@mediatonic.co.uk>
2019-02-07 19:15:58 +0100

    Create audio manager.

---
+++ GGJ19/Assets/Scripts/Audio/PermanentAudioSource.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Audio
+{
+    public class PermanentAudioSource : MonoBehaviour
+    {
+        public static PermanentAudioSource instance;
+
+        private AudioSource _audioSource;
+
+        public void Awake()
+        {
+            _audioSource = GetComponent<AudioSource>();
+
+            if (instance == null)
+            {
+                DontDestroyOnLoad(gameObject);
+                instance = this;
+            }
+            else if (instance != this)
+                Destroy(gameObject);
+        }
+
+        public void PlayOneShot(AudioClip audioClip)
+        {
+            _audioSource.PlayOneShot(audioClip);
+        }
+    }
+}
