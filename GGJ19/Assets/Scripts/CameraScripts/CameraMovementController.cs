b97c5ee27b3f7ea0a11386e652c260b59a7cc4ac
albpb2 <albpb2@gmail.com>
2019-02-16 15:07:09 +0100

    Create script to reset camera position.

---
+++ GGJ19/Assets/Scripts/CameraScripts/CameraMovementController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.CameraScripts
+{
+    public class CameraMovementController : MonoBehaviour
+    {
+        private Vector3 _initialPosition;
+
+        public void ResetPosition()
+        {
+            transform.position = _initialPosition;
+        }
+
+        void Start()
+        {
+            _initialPosition = transform.position;
+        }
+    }
+}
