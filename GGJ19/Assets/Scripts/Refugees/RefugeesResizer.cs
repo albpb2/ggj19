c7d8732eae168747918475f4dbbb184912195100
albpb2 <albpb2@gmail.com>
2019-02-20 08:31:41 +0100

    Resize refugees basing on layer.

---
+++ GGJ19/Assets/Scripts/Refugees/RefugeesResizer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Refugees
+{
+    public class RefugeesResizer
+    {
+        public void ResizeRefugeeBasingOnSortingLayer(Refugee refugee)
+        {
+            var spriteRenderer = refugee.GetComponent<SpriteRenderer>();
+            switch(spriteRenderer.sortingLayerName)
+            {
+                case SortingLayers.Floor2:
+                    refugee.transform.localScale *= 0.95f;
+                    return;
+                case SortingLayers.Floor3:
+                    refugee.transform.localScale *= 0.85f;
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+}
